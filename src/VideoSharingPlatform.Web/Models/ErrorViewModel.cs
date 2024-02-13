1f454c850135397a4d8984acde2796fc1fdc84af
adnan <AdnanGhannam2001>
2024-02-13 23:34:37 +0300

    Solution Init

---
+++ src/VideoSharingPlatform.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VideoSharingPlatform.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
