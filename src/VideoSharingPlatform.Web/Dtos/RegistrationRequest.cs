9998748609678417af5e22be1aba143e51dfecd5
adnan <AdnanGhannam2001>
2024-02-25 14:27:40 +0300

    Add UserDto & Update RegistrationRequest

--- src/VideoSharingPlatform.Web/Dtos/RegistrationRequest.cs
+++ src/VideoSharingPlatform.Web/Dtos/RegistrationRequest.cs
@@ -1,6 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-using System.Runtime.InteropServices;
-
 using VideoSharingPlatform.Application.Features.Commands.CreateUser;
 
 namespace VideoSharingPlatform.Web.Dtos;
