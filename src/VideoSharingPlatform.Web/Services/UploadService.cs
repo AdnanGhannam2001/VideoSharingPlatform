d8741405ed64a5e3e6043390b70ce5b28fd4e165
adnan <AdnanGhannam2001>
2024-02-25 18:21:30 +0300

    Fix Upload Service

--- src/VideoSharingPlatform.Web/Services/UploadService.cs
+++ src/VideoSharingPlatform.Web/Services/UploadService.cs
@@ -5,9 +5,15 @@ namespace VideoSharingPlatform.Web.Services;
 internal class UploadService : IUploadService<IFormFile> {
     private async Task UploadFileAsync(IFormFile file, string path, string name) {
         var ext = Path.GetExtension(file.FileName);
-        var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", path, name + ext);
+        var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", path);
 
-        using var fileStream = new FileStream(fullPath, FileMode.Create);
+        if (!Directory.Exists(fullPath)) {
+            Directory.CreateDirectory(fullPath);
+        }
+
+        var fileName = Path.Combine(fullPath, name + ext);
+
+        using var fileStream = new FileStream(fileName, FileMode.Create);
 
         await file.CopyToAsync(fileStream);
     }
