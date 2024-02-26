e05bd5e71978e2f060fc4079daeff5c897ffd25b
adnan <AdnanGhannam2001>
2024-02-26 07:40:11 +0300

    Fix Handler
    - Inject VideosRepo instead of IReadRepo

--- src/VideoSharingPlatform.Application/Features/Queries/GetVideos/GetVideosHandler.cs
+++ src/VideoSharingPlatform.Application/Features/Queries/GetVideos/GetVideosHandler.cs
@@ -3,15 +3,14 @@ using MediatR;
 using VideoSharingPlatform.Core.Common;
 using VideoSharingPlatform.Core.Entities.VideoAggregate;
 using VideoSharingPlatform.Core.Interfaces;
+using VideoSharingPlatform.Persistent.Data.Repositories;
 
 namespace VideoSharingPlatform.Application.Features.Queries.GetVideos;
 
 public class GetVideosHandler : IRequestHandler<GetVideosQuery, Result<IEnumerable<Video>, IEnumerable<Error>>> {
-    private readonly IReadRepository<Video> _repo;
+    private readonly VideosRepository _repo;
 
-    public GetVideosHandler(IReadRepository<Video> repo) {
-        _repo = repo;
-    }
+    public GetVideosHandler(VideosRepository repo) { _repo = repo; }
 
     public async Task<Result<IEnumerable<Video>, IEnumerable<Error>>> Handle(GetVideosQuery request, CancellationToken cancellationToken) {
         var video = await _repo.GetPageAsync(request.PageNumber,
