75b270bff270546c33aa1b45f0ae83909f84aed2
adnan <AdnanGhannam2001>
2024-02-26 09:25:20 +0300

    Update Videos Repo
    - Override GetByIdAsync

--- src/VideoSharingPlatform.Persistent/Data/Repositories/VideosRepository.cs
+++ src/VideoSharingPlatform.Persistent/Data/Repositories/VideosRepository.cs
@@ -8,6 +8,12 @@ public class VideosRepository : EfRepository<Video>
 {
     public VideosRepository(ApplicationDbContext context) : base(context) { }
 
+    public override Task<Video?> GetByIdAsync<TKey>(TKey id, CancellationToken cancellationToken = default) {
+        return GetQueryable()
+            .Include(x => x.User)
+            .FirstOrDefaultAsync(x => x.Id.Equals(id));
+    }
+
     public override Task<List<Video>> GetPageAsync<TKey>(int pageNumber,
         int pageSize,
         Func<Video, TKey>? keySelector = null,
